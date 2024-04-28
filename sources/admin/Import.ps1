4b3a88b5f751fea4677501dfa62494f645994d10
Sébastien EZRATTY <sebastien.ezratty@kleegroup.com>
2024-04-28 09:02:59 +0200

    Appel de l'API Google Books.

--- sources/admin/Import.ps1
+++ sources/admin/Import.ps1
@@ -1,18 +1,57 @@
 # Chemin du fichier ISBN
 $cheminFichier = "data/definition/isbn.txt"
+$cheminBookApiKey = "sources/admin/booksApi.key"
+
+if (Test-Path $cheminBookApiKey) {
+    # Charge la clé
+    $apiKey = Get-Content $cheminBookApiKey
+} else {
+    Write-Host "Le fichier $cheminBookApiKey n'existe pas."
+    Return
+}
+
+# Fonction pour interroger l'API Google Books pour obtenir les informations du livre par ISBN
+function GetBookInfo($isbn) {
+    $url = "https://www.googleapis.com/books/v1/volumes?q=isbn:$isbn&key=$apiKey"
+    $response = Invoke-RestMethod -Uri $url -Method Get
+    if ($response.totalItems -gt 0) {
+        $bookInfo = $response.items[0].volumeInfo
+        $title = $bookInfo.title
+        $authors = $bookInfo.authors -join ", "
+        $publishedYear = $bookInfo.publishedDate
+        $publisher = $bookInfo.publisher
+        return @{
+            Title = $title
+            Authors = $authors
+            PublishedYear = $publishedYear
+            Publisher = $publisher
+        }
+    } else {
+        return $null
+    }
+}
 
 # Vérifier si le fichier existe
 if (Test-Path $cheminFichier) {
     # Charger les codes ISBN depuis le fichier
     $codesISBN = Get-Content $cheminFichier
 
-    # Afficher les codes ISBN chargés
-    Write-Host "Loaded ISBN codes :"
+    # Parcourir chaque code ISBN
     foreach ($isbn in $codesISBN) {
-        Write-Host $isbn
+        Write-Host "Chargement des informations pour ISBN : $isbn"
+        $bookInfo = GetBookInfo $isbn
+        if ($bookInfo) {
+            # Afficher les informations du livre
+            Write-Host "Titre : $($bookInfo.Title)"
+            Write-Host "Auteurs : $($bookInfo.Authors)"
+            Write-Host "Année d'édition : $($bookInfo.PublishedYear)"
+            Write-Host "Éditeur : $($bookInfo.Publisher)"
+            Write-Host ""
+        } else {
+            Write-Host "Aucune information trouvée pour ISBN : $isbn"
+            Write-Host ""
+        }
     }
-
-    # Vous pouvez utiliser $codesISBN pour effectuer d'autres opérations
 } else {
     Write-Host "Le fichier $cheminFichier n'existe pas."
 }
--- sources/admin/README.md
+++ sources/admin/README.md
@@ -1,5 +1,14 @@
 # Admin
 
+## Présentation
+
 Contient les outils d'administration de la liste des livres.
 
 Import.ps1 est un script qui transforme le fichier data\definition\isbn.txt en data\full\books.json.
+
+## Prérequis
+
+- Activer un compte Google sur Google Developer et se rendre sur https://console.cloud.google.com
+- Créer un projet
+- Créer clé d'API dans le projet et la stocker dans le fichier booksApi.key dans le dossier courant
+- Activer l'API Google Books sur la clé
